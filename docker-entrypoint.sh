05113aa8dc9fa7aa1bd0a1836e6fea12efcb3acb
sandra-forro-dce <saf499@g.harvard.edu>
2025-03-13 01:52:20 +0100

    first add

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,7 +7,7 @@ gcloud auth activate-service-account --key-file=$GOOGLE_APPLICATION_CREDENTIALS
 mkdir -p /mnt/gcs_bucket
 gcsfuse --key-file=$GOOGLE_APPLICATION_CREDENTIALS $GCS_BUCKET_NAME /mnt/gcs_data
 echo 'GCS bucket mounted at /mnt/gcs_data'
-mkdir -p /app/cheese_dataset
-mount --bind /mnt/gcs_data/images /app/cheese_dataset
+mkdir -p /app/rag_dataset
+mount --bind /mnt/gcs_data/rag /app/rag_dataset
 
 pipenv shell
--- docker-shell.sh
+++ docker-shell.sh
@@ -4,9 +4,9 @@ set -e
 
 export BASE_DIR=$(pwd)
 export SECRETS_DIR=$(pwd)/../secrets/
-export GCS_BUCKET_NAME="cheese-app-data-versioning"
-export GCP_PROJECT="ac215-project"
-export GCP_ZONE="us-central1-a"
+export GCS_BUCKET_NAME="couch-gpt-store"
+export GCP_PROJECT="couchgpt-453523"
+export GCP_ZONE="us-central1"
 export GOOGLE_APPLICATION_CREDENTIALS="/secrets/data-service-account.json"
 
 
